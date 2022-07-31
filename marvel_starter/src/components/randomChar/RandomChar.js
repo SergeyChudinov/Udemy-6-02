6b0d7bdc191947a678fe2bac61b168ec9f26262f
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-07-31 13:43:38 +0500

    Make lesson-6-04

--- marvel_starter/src/components/randomChar/RandomChar.js
+++ marvel_starter/src/components/randomChar/RandomChar.js
@@ -1,10 +1,12 @@
 import { Component } from 'react/cjs/react.production.min';
 
 import MarvelService from '../../services/MarvelService';
+import ErroeMessage from '../errorMessage/ErrorMessage';
 
 import './randomChar.scss';
 
 import mjolnir from '../../resources/img/mjolnir.png';
+import Spinner from '../spinner/Spiner';
 
 class RandomChar extends Component {
     constructor(props) {
@@ -12,12 +14,23 @@ class RandomChar extends Component {
         this.updateChar()
     }
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false
     }
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({
+            char,
+            loading: false,
+        });
+    }
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        });
     }
 
     updateChar = () => {
@@ -25,45 +38,16 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     render() {
-        // this.updateChar()
-        let {char: {name, description, thumbnail, homepage, wiki}} = this.state;
-        if (description === '') {
-            description = 'No character data'
-        }
-        if (description !== undefined) {
-            const array = description.split('');
-            if (array.length > 150) {
-                let num = array.length - 150;
-                array.splice(150, num);
-                array.push('...')
-            };
-            description = array.join('');
-        }
-        
-
-
+        let {char, loading, error} = this.state;
+        const errorMessage = error ? <ErroeMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage || spinner || <View char={char}/>}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -82,5 +66,41 @@ class RandomChar extends Component {
     }
 
 }
+const View = ({char}) => {
+    let {name, description, thumbnail, homepage, wiki} = char;
+    if (description === '') {
+        description = 'No character data'
+    }
+    if (description !== undefined) {
+        const array = description.split('');
+        if (array.length > 150) {
+            let num = array.length - 150;
+            array.splice(150, num);
+            array.push('...')
+        };
+        description = array.join('');
+    }
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 export default RandomChar;
